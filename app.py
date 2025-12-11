dcd3c49f3e3b13e33ab81584fced11f7d4090032
rahmon docs <NomharG@MacBook-Pro-9.local>
2025-12-11 02:54:21 -0600

    Import sys for custom transformer module mapping

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/16vduXPKUUBSKaSlaq9nlrAXkqBdn7FRx
 """
 import os
+import sys
 import streamlit as st
 import pandas as pd
 import numpy as np
